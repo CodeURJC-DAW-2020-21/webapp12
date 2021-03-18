6640b741ce38960470d1d710954919ad48cbfc70
kikmar <enriquesantorcazm@gmail.com>
2021-03-18 23:32:51 +0100

    Complete AdminPage

--- UnderSociety/src/main/resources/static/js/demo/chart-pie-demo.js
+++ UnderSociety/src/main/resources/static/js/demo/chart-pie-demo.js
@@ -38,3 +38,38 @@ $.get("/api/admin", function (data, textStatus, jqXHR) {
   });
 
 });
+
+$.get("/api/admin", function (data, textStatus, jqXHR) {
+  users = data;
+  var ctx = document.getElementById("myPieChart2");
+  var myPieChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: ["Posts", "Products"],
+      datasets: [{
+        data: [data.posts, data.products],
+        backgroundColor: ['#4e73df', '#1cc88a'],
+        hoverBackgroundColor: ['#2e59d9', '#17a673'],
+        hoverBorderColor: "rgba(234, 236, 244, 1)",
+      }],
+    },
+    options: {
+      maintainAspectRatio: false,
+      tooltips: {
+        backgroundColor: "rgb(255,255,255)",
+        bodyFontColor: "#858796",
+        borderColor: '#dddfeb',
+        borderWidth: 1,
+        xPadding: 15,
+        yPadding: 15,
+        displayColors: false,
+        caretPadding: 10,
+      },
+      legend: {
+        display: false
+      },
+      cutoutPercentage: 80,
+    },
+  });
+
+});
