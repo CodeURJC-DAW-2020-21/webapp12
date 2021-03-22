49b3df0ff7ea12de12fb6fb1a148d7f8aa1801b7
Antonio <antoniopajueloc21@gmail.com>
2021-03-22 09:35:44 +0100

    FIxe Email

--- UnderSociety/src/main/java/undersociety/controller/EmailController.java
+++ UnderSociety/src/main/java/undersociety/controller/EmailController.java
@@ -1,21 +1,25 @@
 package undersociety.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import undersociety.services.SendMailService;
 
-@Controller
+@RestController
 public class EmailController {
 
 	@Autowired
 	private  SendMailService sendmail;
 	
-	@PostMapping("forgotpasswordmail")
-	public String sendMail(@RequestParam String email) {
-		sendmail.sendMail("undersocietyprueba@gmail.com", email, "Forgot Password", "Forgot password prueba");
-		return "sign-in";
+	@PostMapping("/forgotpasswordmail")
+	public void forgotpasswordmail(@RequestParam String email, HttpServletResponse response) throws IOException {
+		sendmail.sendMailTo("undersocietyprueba@gmail.com", email, "Forgot Password", "Forgot password prueba");
+		response.sendRedirect("/");
 	}
 }
