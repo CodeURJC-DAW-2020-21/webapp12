7efe54c6d0d85e272fdde96c1b97374b99647a0d
David Montero <davidmonterog15@gmail.com>
2021-04-15 18:19:53 +0200

    Implements docker Security

--- Backend/src/main/java/undersociety/configurations/RestSecurityConfig.java
+++ Backend/src/main/java/undersociety/configurations/RestSecurityConfig.java
@@ -1,30 +1,90 @@
 package undersociety.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import undersociety.configurations.jwt.JwtRequestFilter;
 
 @Configuration
 @Order(1)
 public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// Private endpoints
-		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/**").hasRole("USER");
-		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/**").hasRole("USER");
-		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/**").hasRole("USER");
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/**").permitAll();
-		// Disable CSRF protection (it is difficult to implement in REST APIs)
-		http.csrf().ignoringAntMatchers("/api/**");
-		// Disable From Login protection
-		http.formLogin().disable().antMatcher("/api/**");
-		// Enable Basic Authentication
-		http.httpBasic();
-		// Avoid creating session (because every request has credentials) 
+		/*API REST*/
+		http.antMatcher("/api/**");
+
+		/*Users*/
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/users/**").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/users/**").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("USER");
+		
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/relations/**").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/relations/**").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/relations/**").hasRole("USER");
+		
+		/*Post*/
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/posts/**").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/posts/**").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/posts/**").hasRole("USER");
+		
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/likes/**").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/likes/**").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/likes/**").hasRole("USER");
+		
+		/*Products*/
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/bookmarks/**").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/bookmarks/**").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/bookmarks/**").hasRole("USER");
+		
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/products/**").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/products/**").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/products/**").hasRole("USER");
+
+		http.authorizeRequests().anyRequest().permitAll();
+
+		http.csrf().disable();
+
+		http.httpBasic().disable();
+
+		http.formLogin().disable();
+
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 }
