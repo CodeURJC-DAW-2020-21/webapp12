78f58e00000a3a213063018133092616b0590dd1
Antonio <antoniopajueloc21@gmail.com>
2021-04-21 15:35:27 +0200

    Implements Class

--- Frontend/src/app/Class/Bookmarks/bookmarks.ts
+++ Frontend/src/app/Class/Bookmarks/bookmarks.ts
@@ -1,2 +1,38 @@
+import { Product } from "../Product/product";
+import { Users } from "../Users/users";
+
 export class Bookmarks {
+    private idproductlist : Number;
+    private iduser : Users;
+    private idproduct : Product;
+
+	constructor( idproductlist: Number,  iduser: Users,  idproduct: Product) {
+		this.idproductlist =  idproductlist;
+		this.iduser =  iduser;
+		this.idproduct =  idproduct;
+	}
+
+	public getIdproductlist(): Number {
+		return this.idproductlist;
+	}
+
+	public getIduser(): Users {
+		return this.iduser;
+	}
+
+	public getIdproduct(): Product {
+		return this.idproduct;
+	}
+
+	public setIdproductlist(value: Number) {
+		this.idproductlist = value;
+	}
+
+	public setIduser(value: Users) {
+		this.iduser = value;
+	}
+
+	public setIdproduct(value: Product) {
+		this.idproduct = value;
+	}
 }
--- Frontend/src/app/Class/Tags/tags.ts
+++ Frontend/src/app/Class/Tags/tags.ts
@@ -1,2 +1,26 @@
 export class Tags {
+    private idtags : Number;
+    private description : String;
+
+    constructor(idtags : Number, description : String){
+        this.idtags = idtags;
+        this.description = description;
+    }
+
+    public getIdtags(): Number {
+        return this.idtags;
+    }
+
+    public setIdtags(value: Number) {
+        this.idtags = value;
+    }
+
+    public getDescription(): String {
+        return this.description;
+    }
+
+    public setDescription(value: String) {
+        this.description = value;
+    }
+
 }
