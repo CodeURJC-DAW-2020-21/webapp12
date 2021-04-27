573098ecf12fee41baa34ae4ba2b4abc76f83c5b
Antonio <Antoniopajueloc21@gmail.com>
2021-04-27 09:33:59 +0200

    Websocket Function

--- Frontend/src/app/Services/Chat/chat-service.service.ts
+++ Frontend/src/app/Services/Chat/chat-service.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import * as SockJS from 'sockjs-client';
-import * as Stomp from 'stompjs';
+declare var SockJS;
+declare var Stomp;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatServiceService {
 
-  
   constructor() { 
+    this.connect();
   }
 
-  
+  connect() {
+    const sokect = new SockJS('/chat');
+    let stompClient = Stomp.over(sokect);
+    stompClient.connect({}, function(frame) {
+      console.log(frame);
+      stompClient.subscribe('/message/h', (message) => {
+        if (message.body) {
+          let data = JSON.parse(message.body);
+          console.log(data);
+        }
+      });
+    });
+  }
 }
