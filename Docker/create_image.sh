ba9d77abe6f91ad0ea6b768d76a34002a3fa24ca
Antonio <Antoniopajueloc21@gmail.com>
2021-04-29 16:34:35 +0200

    Script update

--- Docker/create_image.sh
+++ Docker/create_image.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 docker=$( cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P )
 backend="$docker/../Backend"
+frontend="$docker/../Frontend"
+cd "$forntend"
+npm install
+ng ng build --prod --base-href="/new/"
 cd "$backend"
-pwd -P
 sudo docker login -u dawpractica21 -p horo.com21
 sudo docker build -t dawpractica21/springboot:spring -f ../Docker/Dockerfile .
 sudo docker push dawpractica21/springboot:spring
